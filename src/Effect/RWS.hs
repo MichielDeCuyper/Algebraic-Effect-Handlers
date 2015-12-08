c0dee878c88730ee25547fe8051263fe95f04e46
Michiel De Cuyper <Michiel.De.Cuyper@Telenet.Be>
2015-12-08 19:59:50 +0100

    changes in RWS, using correct carrier (i think)

--- src/Effect/RWS.hs
+++ src/Effect/RWS.hs
@@ -4,6 +4,7 @@
 {-#LANGUAGE FlexibleInstances #-}
 {-#LANGUAGE FlexibleContexts #-}
 {-#LANGUAGE UndecidableInstances #-}
+{-#LANGUAGE AllowAmbiguousTypes #-}
 
 module Effect.RWS where
 
@@ -24,29 +25,40 @@ instance Functor (RWS r w s) where
     fmap f (Put s k)  = Put s (f k) 
     fmap f (Get k)    = Get (f . k)
 
-newtype RWSCarrier r w s m a = RWSC {unRWS :: r -> s -> m (a, s, w)}
+ask :: (TermMonad m f, RWS r w s :< f) => m r
+ask = inject (Ask var)
+
+tell :: (TermAlgebra h f, RWS r w s :< f) => w -> h ()
+tell w = inject (Tell w (var ()))
+
+get :: (TermMonad h f, RWS r w s :< f) => h s
+get = inject (Get var)
+
+put :: (TermMonad h f, RWS r w s :< f) => s -> h ()
+put s = inject (Put s (var ()))
+
+newtype RWSCarrier r w s m a = RWSC {unRWS :: r -> s -> m (w, a)}
 
 instance Functor m => Functor (RWSCarrier r w s m) where
-    fmap f x = RWSC (fmap (fmap (fmap (lift f))) (unRWS x))
-        where lift :: (a -> b) -> ((a, s, w) -> (b, s, w))
-              lift f (a, b, c) = (f a, b, c)
+    fmap f x = RWSC (fmap (fmap (fmap (fmap f))) (unRWS x))
 
 instance (TermMonad m f, Monoid w) => TermAlgebra (RWSCarrier r w s m) (RWS r w s + f) where
     var = RWSC . genRWS
     con = RWSC . (algRWS \/ conRWS) . fmap unRWS
 
-runRWS :: (Monoid w, TermMonad m f) => Codensity (RWSCarrier r w s m) a -> (r -> s -> m (a, s, w))
+runRWS :: (Monoid w, TermMonad m f) => Codensity (RWSCarrier r w s m) a -> (r -> s -> m (w, a))
 runRWS = unRWS . runCod var
 
-conRWS :: TermMonad m f => f (r -> s -> m (a, s, w)) -> r -> s -> m (a, s, w)
+conRWS :: TermMonad m f => f (r -> s -> m (w, a)) -> r -> s -> m (w, a)
 conRWS op r s = con (fmap (\m -> m r s) op)
 
-genRWS :: (Monoid w, TermMonad m f) => a -> (r -> s -> m (a, s, w))
-genRWS a = (\_ s -> return (a, s, mempty)) -- TODO: Remove s
+genRWS :: (Monoid w, TermMonad m f) => a -> (r -> s -> m (w, a))
+genRWS a = (\_ _ -> return (mempty, a))
 
-algRWS :: RWS r w s (r -> s -> m (a, s, w)) -> (r -> s -> m (a, s, w))
+algRWS :: (Monoid w, TermMonad m f) => RWS r w s (r -> s -> m (w, a)) -> (r -> s -> m (w, a))
 algRWS (Ask g) r s = g r r s
-algRWS (Tell w k) r s = _algTell -- TODO: Find implementation
+algRWS (Tell w k) r s = let a = k r s
+                        in a >>= \(w', x) -> return (w' `mappend` w, x)
 algRWS (Get k) r s = k s r s
-algRWS (Put s' k) r s = k s' r
+algRWS (Put s' k) r s = k r s'
 
--- src/Main.hs
+++ src/Main.hs
@@ -8,43 +8,52 @@ import Data.Codensity
 import Effect.Error
 import Effect.LogState
 import Effect.Nondet
-import Effect.Reader
-import Effect.State
+--import Effect.Reader
+import Effect.RWS
+--import Effect.State
 import Effect.Void
-import Effect.Writer
+--import Effect.Writer
 -- Typeclasses
 import Typeclass.Coproduct
 import Typeclass.TermAlgebra
 import Typeclass.TermMonad
 
-main :: IO ()
-main = print $ show f
-    where f = run $ runState count 100000000
+--main :: IO ()
+--main = print $ show f
+--    where f = run $ runState count 100000000
 
-ex :: TermAlgebra h (Error String + Void) => Int -> h Int
-ex n
-    | n <= 0 = throw "Invalid input"
-    | otherwise = var n
+--ex :: TermAlgebra h (Error String + Void) => Int -> h Int
+--ex n
+--    | n <= 0 = throw "Invalid input"
+--    | otherwise = var n
 
-exa :: TermMonad h (State Int + Writer String + Void) => Int -> h Int
-exa n
-    | n <= 0 = get
-    | otherwise = do a <- get 
-                     put (a+n)
-                     tell ("put value: " ++ show a ++ "+" ++ show n ++ " = " ++ show (a+n) ++ ". ")
-                     exa (n-1)
+--exa :: TermMonad h (State Int + Writer String + Void) => Int -> h Int
+--exa n
+--    | n <= 0 = get
+--    | otherwise = do a <- get 
+--                     put (a+n)
+--                     tell ("put value: " ++ show a ++ "+" ++ show n ++ " = " ++ show (a+n) ++ ". ")
+--                     exa (n-1)
 
-count :: TermMonad h (State Int + Void) => h Int
-count = do i <- get
-           if i == 0 then return i
-           else do put (i - 1)
-                   count
+test :: TermMonad h (RWS String Int String + Void) => Int -> h Int
+test n 
+      | n <= 0 = get
+      | otherwise = do a <- get 
+                       put (a+n)
+                       tell ("put value: " ++ show a ++ "+" ++ show n ++ " = " ++ show (a+n) ++ ". ")
+                       exa (n-1)
 
-greeter :: TermMonad m (Reader String + Void) => m String
-greeter = do name <- ask
-             return ("Hello " ++ name)
+--count :: TermMonad h (State Int + Void) => h Int
+--count = do i <- get
+--           if i == 0 then return i
+--           else do put (i - 1)
+--                   count
+
+--greeter :: TermMonad m (Reader String + Void) => m String
+--greeter = do name <- ask
+--             return ("Hello " ++ name)
 
 
-catch :: (TermMonad h f, Error e :< f) => h a -> (e -> h a) -> h a
-catch = undefined
+--catch :: (TermMonad h f, Error e :< f) => h a -> (e -> h a) -> h a
+--catch = undefined
 
